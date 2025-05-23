f46f7a24902772ec15fbd0e5cb0d1824ef7f2dd2
wade.lewis+convertkit@gmail.com <wade.lewis+convertkit@gmail.com>
2025-05-22 22:11:15 -0500

    Add Express.js app with CI/CD pipeline

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post('/api/users', (req, res) => {
 });
 
 // Error handling middleware
-app.use((err, req, res, next) => {
+app.use((err, req, res, _next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
